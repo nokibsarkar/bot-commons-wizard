3924e6b26f70c6612f4d78db6fece8bdc718297a
Nazmul Haque Naqib <nokibsarkar@gmail.com>
2023-11-19 18:57:57 +0600

    details file is complete

--- frontend/src/Components/WizardBuilder/Details.jsx
+++ frontend/src/Components/WizardBuilder/Details.jsx
@@ -22,22 +22,25 @@ const CampaignDetails = ({ state, dispatch }) => {
                     value={state.name}
                     data-testid="campaign-name-input"
                     onChange={(e) => dispatch({ type: "name", payload: e.target.value })}
+                    required
                 />
             </fieldset>
             <WikiTextParser campaignName={state.name} fieldName="Title" onChooseTemplate={(wikitext) => dispatch({ type: "config.title", payload: wikitext })} placeholder={`Default is "${state.name}"`} />
             <WikiTextParser campaignName={state.name} fieldName="Description" onChooseTemplate={(wikitext) => dispatch({ type: "config.description", payload: wikitext })} />
             <WikiTextParser campaignName={state.name} fieldName="Campaign Banner" onChooseTemplate={(wikitext) => dispatch({ type: "config.display.headerLabel", payload: wikitext })} />
             <fieldset className="flex flex-row  border-solid border-gray-600 border p-5">
-                <legend className="text-lg">Campaign Duration</legend>
+                <legend className="text-lg" data-testid="details-campaign-duration-legend" >Campaign Duration</legend>
                 <input
+                    data-testid="details-start-date"
                     className="border border-gray-400 rounded-lg px-4 py-2 m-2"
                     type="date" value={state.startDate} onChange={(e) => dispatch({ type: "config.start", payload: e.target.value })} />
                 <span className="mx-2">to</span>
                 <input
+                    data-testid="details-end-date"
                     className="border border-gray-400 rounded-lg px-4 py-2 m-2"
                     type="date" value={state.endDate} onChange={(e) => dispatch({ type: "config.end", payload: e.target.value })} />
             </fieldset>
-            <button type="button" disabled={!readyToGotNext} className="border disabled:bg-opacity-70 text-white bg-green-700 w-1/2 border-gray-400 rounded-lg px-4 py-2 m-2" onClick={() => dispatch({ type: "step", payload: 1 })}>
+            <button data-testid="details-next-button" type="button" disabled={!readyToGotNext} className="border disabled:bg-opacity-70 text-white bg-green-700 w-1/2 border-gray-400 rounded-lg px-4 py-2 m-2" onClick={() => dispatch({ type: "step", payload: 1 })}>
                 Add Tutorial Settings
             </button>
         </div>
--- frontend/src/tests/Details.test.js
+++ frontend/src/tests/Details.test.js
@@ -2,8 +2,18 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
 import { act } from 'react-dom/test-utils';
 import setup from './App.test';
+const setupAndSelectStep = (step = 0) => {
+    const utils = setup(step);
+    const nextButton = screen.getByTestId("details-next-button");
+    expect(nextButton).toBeInTheDocument();
+    // expect(nextButton).toBeDisabled();
+    return {
+        nextButton,
+        ...utils,
+    }
+}
 test("Render Campaign Name Input", async () => {
-    setup(0);
+    const { nextButton } = setupAndSelectStep();
     const campaignName = screen.getByTestId("campaign-name");
     expect(campaignName).toBeInTheDocument();
     const campaignNameLabel = screen.getByTestId("campaign-name-helper");
@@ -20,23 +30,35 @@ test("Render Campaign Name Input", async () => {
     expect(campaignNameInput.value).toBe("Test Campaign");
     act(() => campaignNameInput.blur());
     expect(campaignNameInput).not.toHaveFocus();
+    // check this is required
+    expect(campaignNameInput).toBeRequired();
+    // empty it and check it is invalid
+    fireEvent.change(campaignNameInput, { target: { value: "" } });
+    expect(campaignNameInput.value).toBe("");
+    expect(campaignNameInput).toBeInvalid();
+    expect(nextButton).toBeDisabled();
 });
 test("Render Campaign Title Input", async () => {
+    const {nextButton}    = setupAndSelectStep();
     const testCampaignName = "Test Campaign";
-    setup(0);
     const campaignTitle = screen.getByText("Title");
     expect(campaignTitle).toBeInTheDocument();
     const campaignTitleLegend = screen.getByTestId("Title-legend");
     expect(campaignTitleLegend).toBeInTheDocument();
     const campaignTitleLabel = screen.getByTestId("Title-helper");
     expect(campaignTitleLabel).toBeInTheDocument();
-    
+
     expect(campaignTitleLegend).toHaveTextContent("Title");
     const campaignTitleInput = screen.getByTestId("Title-input");
     expect(campaignTitleInput).toBeInTheDocument();
     expect(campaignTitleInput).toHaveAttribute("type", "text");
     const campaignNameInput = screen.getByTestId("campaign-name-input");
-    fireEvent.change(campaignNameInput, { target: { value: testCampaignName } });
+
+    // By default, the campaign title is ""
+    expect(campaignTitleInput.value).toBe("");
+    expect(nextButton).not.toBeDisabled(); // Because the campaign title is optional
+    act(() => fireEvent.change(campaignNameInput, { target: { value: testCampaignName } }));
+
     expect(campaignNameInput.value).toBe(testCampaignName);
     expect(campaignTitleInput).toHaveAttribute("placeholder", `Default is "${testCampaignName}"`);
     campaignTitleInput.focus();
@@ -45,9 +67,10 @@ test("Render Campaign Title Input", async () => {
     expect(campaignTitleInput.value).toBe(testCampaignName);
     campaignTitleInput.blur();
     expect(campaignTitleInput).not.toHaveFocus();
+    expect(nextButton).not.toBeDisabled(); // Because the campaign title is optional
 });
 test("Render Campaign Description Input", async () => {
-    setup(0);
+    const {nextButton} =  setupAndSelectStep();
     const campaignDescription = screen.getByText("Description")
     expect(campaignDescription).toBeInTheDocument();
     const campaignDescriptionLegend = screen.getByTestId("Description-legend");
@@ -58,10 +81,85 @@ test("Render Campaign Description Input", async () => {
     const campaignDescriptionInput = screen.getByTestId("Description-input");
     expect(campaignDescriptionInput).toBeInTheDocument();
     expect(campaignDescriptionInput).toHaveAttribute("type", "text");
+    // By default, the campaign description is ""
+    expect(campaignDescriptionInput.value).toBe("");
+    expect(nextButton).not.toBeDisabled(); // Because the campaign description is optional
     campaignDescriptionInput.focus();
     expect(campaignDescriptionInput).toHaveFocus();
     fireEvent.change(campaignDescriptionInput, { target: { value: "Any Campaign Description" } });
     expect(campaignDescriptionInput.value).toBe("Any Campaign Description");
     campaignDescriptionInput.blur();
     expect(campaignDescriptionInput).not.toHaveFocus();
+    expect(nextButton).not.toBeDisabled(); // Because the campaign description is optional
+});
+test("Render Campaign Banner Input", async () => {
+    const {nextButton} =  setupAndSelectStep();
+    const campaignBanner = screen.getByText("Campaign Banner")
+    expect(campaignBanner).toBeInTheDocument();
+    const campaignBannerLegend = screen.getByTestId("Campaign Banner-legend");
+    expect(campaignBannerLegend).toBeInTheDocument();
+    const campaignBannerLabel = screen.getByTestId("Campaign Banner-helper");
+    expect(campaignBannerLabel).toBeInTheDocument();
+    expect(campaignBannerLegend).toHaveTextContent("Campaign Banner");
+    const campaignBannerInput = screen.getByTestId("Campaign Banner-input");
+    expect(campaignBannerInput).toBeInTheDocument();
+    expect(campaignBannerInput).toHaveAttribute("type", "text");
+    campaignBannerInput.focus();
+    // By default, the campaign banner is ""
+    expect(campaignBannerInput.value).toBe("");
+    expect(nextButton).not.toBeDisabled(); // Because the campaign banner is optional
+    expect(campaignBannerInput).toHaveFocus();
+    fireEvent.change(campaignBannerInput, { target: { value: "Any Campaign Banner" } });
+    expect(campaignBannerInput.value).toBe("Any Campaign Banner");
+    campaignBannerInput.blur();
+    expect(campaignBannerInput).not.toHaveFocus();
+    expect(nextButton).not.toBeDisabled(); // Because the campaign banner is optional
+});
+test("Render Campaign Start Date Input", async () => {
+    const {nextButton} =  setupAndSelectStep();
+    const campaignStartDate = screen.getByTestId('details-campaign-duration-legend');
+    expect(campaignStartDate).toBeInTheDocument();
+    expect(campaignStartDate).toHaveTextContent("Campaign Duration");
+
+    const campaignStartDateInput = screen.getByTestId("details-start-date");
+    expect(campaignStartDateInput).toBeInTheDocument();
+    expect(campaignStartDateInput).toHaveAttribute("type", "date");
+    campaignStartDateInput.focus();
+    expect(campaignStartDateInput).toHaveFocus();
+    // By default, the campaign start date is ""
+    expect(campaignStartDateInput.value).toBe("");
+    expect(nextButton).not.toBeDisabled(); // Because the campaign start date is optional
+    fireEvent.change(campaignStartDateInput, { target: { value: "2021-01-01" } });
+    expect(campaignStartDateInput.value).toBe("2021-01-01");
+    campaignStartDateInput.blur();
+    expect(campaignStartDateInput).not.toHaveFocus();
+    expect(nextButton).not.toBeDisabled(); // Because the campaign start date is optional
+});
+test("Render Campaign End Date Input", async () => {
+    const {nextButton} =  setupAndSelectStep();
+    const campaignEndDate = screen.getByTestId('details-campaign-duration-legend');
+    expect(campaignEndDate).toBeInTheDocument();
+    expect(campaignEndDate).toHaveTextContent("Campaign Duration");
+    const campaignEndDateInput = screen.getByTestId("details-end-date");
+    expect(campaignEndDateInput).toBeInTheDocument();
+    expect(campaignEndDateInput).toHaveAttribute("type", "date");
+    campaignEndDateInput.focus();
+    expect(campaignEndDateInput).toHaveFocus();
+    // By default, the campaign end date is ""
+    expect(campaignEndDateInput.value).toBe("");
+    expect(nextButton).not.toBeDisabled(); // Because the campaign end date is optional
+    fireEvent.change(campaignEndDateInput, { target: { value: "2021-01-01" } });
+    expect(campaignEndDateInput.value).toBe("2021-01-01");
+    campaignEndDateInput.blur();
+    expect(campaignEndDateInput).not.toHaveFocus();
+    expect(nextButton).not.toBeDisabled(); // Because the campaign end date is optional
+});
+test("Render Next Button", async () => {
+    const {nextButton, stepper} =  setupAndSelectStep();
+    expect(nextButton).not.toHaveTextContent("");
+    expect(nextButton).toBeEnabled();
+    act(() => nextButton.click());
+    expect(nextButton).not.toBeInTheDocument();
+    const nextStep = stepper.children[1];
+    expect(nextStep).toHaveAttribute("data-selected", "true");
 });
